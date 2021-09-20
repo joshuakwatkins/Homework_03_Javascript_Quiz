74f90db43791284ba05381470d7213a7967b27d4
Josh Watkins <joshwatkinssfs@gmail.com>
2021-09-20 16:39:34 -0400

    uploading my html and css skeleton

---
+++ Assets/JS/script.js
@@ -0,0 +1,19 @@
+
+
+
+
+// on start, begin a set interval for 75 seconds and advance the index a position to show which card
+
+// sooo for every question (stored as a list within an object? or as an array?)
+// if (event.target.hasAttribute.dataSet.correct)
+// then advance to the next card, and display correct/incorect for an interval.
+// also probably i want include a running score
+// else we subtract 10 from the interval and display the next card.
+// repeat for questions 2 through 5.
+// upon selecting an answer for question 5, the timer stops
+// and you are presented your final score and are prompted to enter your initials for your high score.
+// your high score is then appended to an object containing 2 arrays,
+// one for the initials input and one for the score, and stored in local storage
+// the scoreboard is then procedurally generated using a for loop
+// and some conditional css formatting (nth child?)
+// 

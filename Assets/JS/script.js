62b65c1694e17e48335a2d338533a72a40a203c1
Josh Watkins <joshwatkinssfs@gmail.com>
2021-09-22 10:15:20 -0400

    mostly there - still a weird iterative array object pushing issue where the Nth playhtrough gives N high score copies

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,23 +1,4 @@
 
-
-
-
-// on start, begin a set interval for 75 seconds and advance the index a position to show which card
-
-// sooo for every question (stored as a list within an object? or as an array?)
-// if (event.target.hasAttribute.dataSet.correct)
-// then advance to the next card, and display correct/incorect for an interval.
-// also probably i want include a running score
-// else we subtract 10 from the interval and display the next card.
-// repeat for questions 2 through 5.
-// upon selecting an answer for question 5, the timer stops
-// and you are presented your final score and are prompted to enter your initials for your high score.
-// your high score is then appended to an object containing 2 arrays,
-// one for the initials input and one for the score, and stored in local storage
-// the scoreboard is then procedurally generated using a for loop
-// and some conditional css formatting (nth child?)
-// 
-
 var question0 = document.querySelector("#question-0");
 var question1 = document.querySelector("#question-1");
 var question2 = document.querySelector("#question-2");
@@ -28,6 +9,8 @@ var question6 = document.querySelector("#question-6");
 var question7 = document.querySelector("#question-7");
 
 var highScoreList = document.querySelector("#high-score-list");
+
+var feedback = document.querySelector("#answer-response");
 
 function readyGame () {
 question0.style.display = "block";
@@ -50,13 +33,14 @@ var highScores = [
 
 function init() {
     readyGame();
-    getHighscores(highScores);
-    setHighscores();
+
     
 }
+var gameTimeCountdown;
 var finalScore;
 var userName;
 var gameTime = 750;
+var newScore = {};
 var isDone = false;
 var q1 = true;
 var q2 = true;
@@ -66,7 +50,9 @@ var q5 = true;
 
 function countdown() {
     var timer = document.querySelector("#timer-countdown");
-    var gameTimeCountdown = setInterval(function () {
+    timer.textContent = "";
+    // gameTime = 750;
+    gameTimeCountdown = setInterval(function () {
     gameTime--;
     timer.textContent = "TIME: " + gameTime;
     if (gameTime <= 0) {
@@ -94,80 +80,229 @@ function countdown() {
 };
 
 function quiz () {
+    gameTime = 750;
     countdown();
     question0.style.display = "none";
     question1.style.display = "block";
     for (i=1;i<5;i++){
         var button = document.getElementsByClassName("button"+i);
-        console.log(button[0]);
         button[0].addEventListener("click", answerCheckOne)
     }
-    // var questionTarget1 = question1.getElementsByClassName("button");
-    // questionTarget1.addEventListener("click", answerCheckOne);
     function answerCheckOne(event) {
-        console.log(event);
+        // console.log(event);
         if (event.target.hasAttribute("id","correct")) {
             toQuestion2();
+            feedback.textContent="";
+            var feedbackTime = 100;
+            var feedbackTimer = setInterval(function() {
+                feedbackTime--;
+                if (feedbackTime > 0) { 
+                    feedback.textContent=" - - - C o r r e c t - - -";
+                    // feedback.textContent=" - - - - W R O N G  Y A  D U M M Y - - - - "
+                } else if (feedbackTime <= 0) {
+                    feedback.textContent=" ";
+                    clearInterval(feedbackTimer);
+                }
+            },10);
         } else {
             q1 = false;
             toQuestion2();
+            feedback.textContent="";
+            var feedbackTime = 100;
+            var feedbackTimer = setInterval(function() {
+                feedbackTime--;
+                if (feedbackTime > 0) { 
+                    // feedback.textContent=" - - - C o r r e c t - - -";
+                    feedback.textContent=" - - - - W R O N G  Y A  D U M M Y - - - - "
+                } else if (feedbackTime <= 0) {
+                    feedback.textContent=" ";
+                    clearInterval(feedbackTimer);
+                }
+            },10);
         }
     }
     console.log("question 1 is working");
     function toQuestion2() {
         question1.style.display = "none";
         question2.style.display = "block";
-        question2.addEventListener("click", toQuestion3)
-        console.log("question 2 is working");
-
+        question2.addEventListener("click", answerCheckTwo)
+        // console.log("question 2 is working");
+    }
+    function answerCheckTwo(event) {
+        // console.log(event);
+        if(event.target.hasAttribute("id","correct")) {
+            toQuestion3()
+            feedback.textContent="";
+            var feedbackTime = 100;
+            var feedbackTimer = setInterval(function() {
+                feedbackTime--;
+                if (feedbackTime > 0) { 
+                    feedback.textContent=" - - - C o r r e c t - - -";
+                    // feedback.textContent=" - - - - W R O N G  Y A  D U M M Y - - - - "
+                } else if (feedbackTime <= 0) {
+                    feedback.textContent=" ";
+                    clearInterval(feedbackTimer);
+                }
+            },10);
+        } else {
+            q2 = false;
+            toQuestion3();
+            feedback.textContent="";
+            var feedbackTime = 100;
+            var feedbackTimer = setInterval(function() {
+                feedbackTime--;
+                if (feedbackTime > 0) { 
+                    // feedback.textContent=" - - - C o r r e c t - - -";
+                    feedback.textContent=" - - - - W R O N G  Y A  D U M M Y - - - - "
+                } else if (feedbackTime <= 0) {
+                    feedback.textContent=" ";
+                    clearInterval(feedbackTimer);
+                }
+            },10);
+        }
     }
     function toQuestion3() {
         question2.style.display = "none";
         question3.style.display = "block";
-        question3.addEventListener("click", toQuestion4)
-        console.log("question 3 is working");
+        question3.addEventListener("click", answerCheckThree)
+        // console.log("question 3 is working");
 
+    }
+    function answerCheckThree(event) {
+        // console.log(event);
+        if(event.target.hasAttribute("id","correct")) {
+            toQuestion4();
+            feedback.textContent="";
+            var feedbackTime = 100;
+            var feedbackTimer = setInterval(function() {
+                feedbackTime--;
+                if (feedbackTime > 0) { 
+                    feedback.textContent=" - - - C o r r e c t - - -";
+                    // feedback.textContent=" - - - - W R O N G  Y A  D U M M Y - - - - "
+                } else if (feedbackTime <= 0) {
+                    feedback.textContent=" ";
+                    clearInterval(feedbackTimer);
+                }
+            },10);
+        } else {
+            q3 = false;
+            toQuestion4();
+            var feedbackTime = 100;
+            var feedbackTimer = setInterval(function() {
+                feedbackTime--;
+                if (feedbackTime > 0) { 
+                    // feedback.textContent=" - - - C o r r e c t - - -";
+                    feedback.textContent=" - - - - W R O N G  Y A  D U M M Y - - - - "
+                } else if (feedbackTime <= 0) {
+                    feedback.textContent=" ";
+                    clearInterval(feedbackTimer);
+                }
+            },10);
+        }
     }
     function toQuestion4() {
         question3.style.display = "none";
         question4.style.display = "block";
-        question4.addEventListener("click", toQuestion5)
-        console.log("question 4 is working");
+        question4.addEventListener("click", answerCheckFour)
+        // console.log("question 4 is working");
+    }
+    function answerCheckFour(event) {
+        console.log(event);
+        if(event.target.hasAttribute("id","correct")) {
+            toQuestion5();
+            var feedbackTime = 100;
+            var feedbackTimer = setInterval(function() {
+                feedbackTime--;
+                if (feedbackTime > 0) { 
+                    feedback.textContent=" - - - C o r r e c t - - -";
+                    // feedback.textContent=" - - - - W R O N G  Y A  D U M M Y - - - - "
+                } else if (feedbackTime <= 0) {
+                    feedback.textContent=" ";
+                    clearInterval(feedbackTimer);
+                }
+            },10);
+        } else {
+            q4 = false;
+            toQuestion5();
+            var feedbackTime = 100;
+            var feedbackTimer = setInterval(function() {
+                feedbackTime--;
+                if (feedbackTime > 0) { 
+                    // feedback.textContent=" - - - C o r r e c t - - -";
+                    feedback.textContent=" - - - - W R O N G  Y A  D U M M Y - - - - "
+                } else if (feedbackTime <= 0) {
+                    feedback.textContent=" ";
+                    clearInterval(feedbackTimer);
+                }
+            },10);
+        }
     }
     function toQuestion5() {
         question4.style.display = "none";
         question5.style.display = "block";
-        question5.addEventListener("click", toQuestion6)
+        question5.addEventListener("click", answerCheckFive)
         console.log("question 5 is working");
 
     }
+    function answerCheckFive(event) {
+        console.log(event);
+        if(event.target.hasAttribute("id","correct")) {
+            toQuestion6();
+            var feedbackTime = 100;
+            var feedbackTimer = setInterval(function() {
+                feedbackTime--;
+                if (feedbackTime > 0) { 
+                    feedback.textContent=" - - - C o r r e c t - - -";
+                    // feedback.textContent=" - - - - W R O N G  Y A  D U M M Y - - - - "
+                } else if (feedbackTime <= 0) {
+                    feedback.textContent=" ";
+                    clearInterval(feedbackTimer);
+                }
+            },10);
+        } else {
+            q5 = false;
+            toQuestion6();
+            var feedbackTime = 100;
+            var feedbackTimer = setInterval(function() {
+                feedbackTime--;
+                if (feedbackTime > 0) { 
+                    // feedback.textContent=" - - - C o r r e c t - - -";
+                    feedback.textContent=" - - - - W R O N G  Y A  D U M M Y - - - - "
+                } else if (feedbackTime <= 0) {
+                    feedback.textContent=" ";
+                    clearInterval(feedbackTimer);
+                }
+            },10);
+        }
+    }
     function toQuestion6() {
-        console.log("beginning of q6");
         question5.style.display = "none";
         question6.style.display = "block";
         isDone = true;
-        console.log("middle of q6");
         var finalScore = gameTime;
         document.querySelector("#final-score").textContent = "Congratulations! You scored " + finalScore + "points! Enter your initials for the High Score board!";
-        console.log("end of q6");
         document.querySelector("#userIDSubmit").addEventListener("click", submitHS);
     }
     function submitHS(event) {
-        event.preventDefault;
+        isDone = false;
+        event.preventDefault();
+        setHighscores();
         console.log(highScores)
         console.log("beginning of q7");
-        userName = document.querySelector("#userID").value.toUpperCase();
-        var newScore = {
-            userID: userName,
-            userScore: gameTime,
-        };
-        console.log(newScore);
-        highScores.push(newScore);
-        console.log(highScores.length);
-        setHighscores();
+        // userName = document.querySelector("#userID").value.toUpperCase();
+        // // var restartBtn = document.querySelector("#restart-quiz");
+        // // restartBtn.addEventListener("click", readyGame);
+        // newScore = {
+        //     userID: userName,
+        //     userScore: gameTime,
+        // };
+        // console.log(highScores);
+        // console.log(newScore);
+        // highScores.push(newScore);
+        // console.log(highScores);
         // getHighscores();
-        viewHighScore();
-        console.log("final step");
+
+        // console.log("final step");
     }
 };
     
@@ -177,8 +312,6 @@ question0.addEventListener("click", quiz);
 document.querySelector("#highScoreButton").addEventListener("click",viewHighScore)
 
 function viewHighScore () {
-    // setHighscores();
-    getHighscores(highScores);
     question0.style.display = "none";
     question1.style.display = "none";
     question2.style.display = "none";
@@ -187,18 +320,39 @@ function viewHighScore () {
     question5.style.display = "none";
     question6.style.display = "none";
     question7.style.display = "block";
+    getHighscores(highScores);
+    var restartBtn = document.querySelector("#restart-quiz");
+    restartBtn.addEventListener("click", readyGame);
     return;
 }
 
 function setHighscores () {
-    
+    userName = document.querySelector("#userID").value.toUpperCase();
+    // var restartBtn = document.querySelector("#restart-quiz");
+    // restartBtn.addEventListener("click", readyGame);
+    highScores = JSON.parse(localStorage.getItem("highScores"));
+    if (highScores === null) {
+        highScores = [];
+    } else {
+    newScore = {
+        userID: userName,
+        userScore: gameTime,
+    };
+    // console.log(highScores);
+    // console.log(newScore);
+    highScores.push(newScore);
+    // console.log(highScores);
     localStorage.setItem("highScores", JSON.stringify(highScores));
-    console.log("THIS IS MY SET ITEM FOR THE HIGH SCORE ITEM ARRAY" + localStorage.getItem("highScores"));
+    newScore = {};
+    console.log(highScores);
+    viewHighScore();
+}
 };
 
 function getHighscores(highScores) {
    highScores = JSON.parse(localStorage.getItem("highScores"));
    if (highScores === null) {
+       highScores = [];
        return;
    } else {
        highScores.sort((a , b) => {
@@ -209,12 +363,12 @@ function getHighscores(highScores) {
     console.log(highScores.length);
         for (i=0; i<highScores.length; i++) {
             var highScore = `${highScores[i].userID} - ${highScores[i].userScore}`;
+            // var highScore = highScores[i].userId + " - " + highScores[i].userScore;
             var li = document.createElement("li");
             li.textContent = highScore;
-            li.setAttribute("data-index", i);
             highScoreList.appendChild(li);
             } 
-            }
-        };
+        }
+    };
 
 init();
